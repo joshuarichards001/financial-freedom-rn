d3028c9837c1ae0bc8b0dd5710c661f60814e159
josh richards <joshuarichards001@gmail.com>
2020-09-20 14:07:56 +1200

    Added data vis

--- src/components/CategoryPieChart.js
+++ src/components/CategoryPieChart.js
@@ -1,10 +1,41 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {VictoryPie} from 'victory-native';
 
-export default function CategoryPieChart() {
+export default function CategoryPieChart({transactionList}) {
+  let categoryMap = expenseCategories();
+
+  // calculates how much the user has spent on each category
+  function expenseCategories() {
+    let map = new Map();
+    for (let transaction of transactionList) {
+      if (!transaction.income) {
+        const amount = Number(transaction.amount);
+        if (map.has(transaction.category)) {
+          map.set(
+            transaction.category,
+            Number(map.get(transaction.category)) + amount,
+          );
+        } else {
+          map.set(transaction.category, amount);
+        }
+      }
+    }
+    let output = [];
+    for (let [key, value] of map) {
+      output.push({x: key, y: value});
+    }
+    return output;
+  }
+
   return (
-    <View>
-      <Text></Text>
+    <View style={{borderWidth: 1}}>
+      <VictoryPie
+        colorScale={['red', 'green', 'gold', 'cyan', 'navy']}
+        data={categoryMap}
+        width={250}
+        height={250}
+      />
     </View>
   );
 }
--- src/components/FlowPieChart.js
+++ src/components/FlowPieChart.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {VictoryPie} from 'victory-native';
 
-export default function FlowPieChart() {
+export default function FlowPieChart({transactionList}) {
+  const inc = incomeExpense()[0];
+  const exp = incomeExpense()[1];
+
+  // calculates how much the user earns vs how much they spend
+  function incomeExpense() {
+    var income = 0;
+    var expense = 0;
+    for (let transaction of transactionList) {
+      const amount = Number(transaction.amount);
+      if (transaction.income) {
+        income = income + amount;
+      } else {
+        expense = expense + amount;
+      }
+    }
+    return [income, expense];
+  }
+
   return (
-    <View>
-      <Text></Text>
+    <View style={{borderWidth: 1}}>
+      <VictoryPie
+        colorScale={['red', 'green', 'gold', 'cyan', 'navy']}
+        data={[
+          {x: 'Income', y: inc},
+          {x: 'Expense', y: exp},
+        ]}
+        width={250}
+        height={250}
+      />
     </View>
   );
 }
